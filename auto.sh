be3efa074b0a0990f70eabdb9858789f03f6c699
Faizan quazi <faizan_quazi@intuit.com>
2024-10-30 16:08:38 +0530

    obasfucate

--- auto.sh
+++ auto.sh
@@ -16,7 +16,7 @@ docker build -t dexterquazi/digitalmosque  --no-cache .
 docker push dexterquazi/digitalmosque
 docker run -itd --name mosque -p 80:80 -p 443:443 dexterquazi/digitalmosque
 else
-return 0
+echo "No Ch"
 fi
 sleep 5
 
