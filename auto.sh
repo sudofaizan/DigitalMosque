0595eb4c7343983c406643ef067446acebdf2917
Faizan quazi <faizan_quazi@intuit.com>
2024-10-30 15:55:39 +0530

    obasfucate

--- auto.sh
+++ auto.sh
@@ -1,5 +1,8 @@
-git pull
+cd /home/ec2-user/DigitalMosque
+if git pull |grep changed
+then
 docker rm -f $(docker ps -aq)
 docker build -t dexterquazi/digitalmosque  --no-cache .
 docker push dexterquazi/digitalmosque
 docker run -itd --name mosque -p 80:80 -p 443:443 dexterquazi/digitalmosque
+fi
