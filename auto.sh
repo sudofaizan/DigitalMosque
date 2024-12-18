5e9925c5c4fdd96552d9df69d616438b43afe21b
Faizan quazi <faizan_quazi@intuit.com>
2024-12-18 15:56:01 +0530

    upd

--- auto.sh
+++ auto.sh
@@ -1,38 +1,12 @@
-#!/bin/bash
-# Define paths for reusability
-SERVICE_PATH="/etc/systemd/system/mosque.service"
-SERVICE_NAME="mosque"
-DOCKER_IMAGE="dexterquazi/digitalmosque"
-LOCAL_DIR="/home/ec2-user/DigitalMosque"
-
-# Check if the systemd service is present
-if [[ -f $SERVICE_PATH ]]; then
-    echo "Daemon present"
-else
-    # Copy service file and start the service
-    cp "$LOCAL_DIR/$SERVICE_NAME.service" "$SERVICE_PATH"
-    sudo systemctl daemon-reload
-    sudo systemctl start "$SERVICE_NAME"
-    sudo systemctl enable "$SERVICE_NAME"
-fi
-
-# Pull latest code and check for changes
-cd "$LOCAL_DIR"
-git stash
-
-if git pull | grep -q "changed"; then
-    # Remove all running containers
-    docker rm -f $(docker ps -aq)
-
-    # Build and push the Docker image
-    docker build -t "$DOCKER_IMAGE" --no-cache .
-    docker push "$DOCKER_IMAGE"
-
-    # Run the Docker container
-    docker run -d --name "$SERVICE_NAME" -p 80:80 -p 443:443 "$DOCKER_IMAGE"
-else
-    echo "No changes detected"
-fi
-
-# Delay before re-checking
-sleep 600
+cd /home/ec2-user/DigitalMosque
+update(){
+if git pull --rebase --force|grep Updating
+then
+docker rm -f mosque
+docker rmi -f mosque
+docker build -t mosque .
+docker run -it --name mosque -p 80:80 -p 443:443 mosque
+else 
+sleep 500
+update
+}
