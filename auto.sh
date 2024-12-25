7eb6ac0aed6c5378763e8d82db14af799fc49f22
Faizan quazi <faizan_quazi@intuit.com>
2024-12-25 22:31:02 +0530

    qari change

--- auto.sh
+++ auto.sh
@@ -1,19 +1,13 @@
 cd /home/ec2-user/DigitalMosque
-docker build -t mosque .
-docker run -itd --name mosque -p 80:80 -p 443:443 mosque
-update(){
-if git pull --rebase --force|grep Updating
-then
+# docker build -t mosque .
+# docker run -itd --name mosque -p 80:80 -p 443:443 mosque
+
+# update(){
+# if git pull --rebase --force|grep Updating
+# then
 docker rm -f mosque
 docker rmi -f mosque
-docker build -t mosque .
+docker build -t mosque . --no-cache
 docker run -d --name mosque -p 80:80 -p 443:443 mosque
 bash purge.sh
 echo "purged"
-update
-else 
-sleep 5
-update
-fi
-}
-update
