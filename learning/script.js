f60697e59f6512d94a33aefd8480c2672fe64115
Faizan quazi <faizan_quazi@intuit.com>
2025-01-01 13:15:11 +0530

    qibla

--- learning/script.js
+++ learning/script.js
@@ -1,7 +1,8 @@
 document.querySelectorAll('.alphabet').forEach(alphabet => {
     alphabet.addEventListener('mouseover', () => {
       const sound = alphabet.getAttribute('data-sound');
-      const audio = new Audio(`audio/${sound}`);
+      console.log(sound); 
+      const audio = new Audio(`${sound}`);
       audio.play();
     });
   });
